6d00b33df420cb44a7e57de45282cbe666fb1598
Lee Grey <leegrey.nz@gmail.com>
2025-08-26 20:52:30 +1200

    remove debug log

--- rpgbuild.js
+++ rpgbuild.js
@@ -635,7 +635,6 @@ function collectValidLines(lines, startIndex, endIndex) {
     }
 
     if (shuffleItems) {
-        console.log("Shuffling items");
         shuffleArray(validLines);
     }
      
@@ -858,7 +857,6 @@ function generateSequentialNumberedTable(lines, lineIndex, tableLines, columns =
 }
 
 function shuffleArray(array) {
-    console.log(array)
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
